d2e7b5c3d8fcc211cd1eb856f5c89661f8611cfd
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2023-04-22 18:57:45 -0300

    .

---
+++ src/modules/user/dto/CreateUserDTO.ts
@@ -0,0 +1,5 @@
+export interface CreateUserDTO {
+    name: String,
+    email: String,
+    password: String
+}
---
+++ src/modules/user/userCase/createUser/CreateUserUseCase.ts
@@ -0,0 +1,4 @@
+// import { PrismaClient } from '@prisma/client'
+// import { prisma } from '../../../../prisma/client/PrismaClient'
+
+// prisma.user
