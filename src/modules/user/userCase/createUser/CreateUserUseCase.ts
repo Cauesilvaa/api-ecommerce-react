dc6bc67cdcb58cab8b842b47690b935183a9bb62
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2023-04-22 23:53:28 -0300

    criação de usuario feita

--- src/modules/user/userCase/createUser/CreateUserUseCase.ts
+++ src/modules/user/userCase/createUser/CreateUserUseCase.ts
@@ -1,4 +1,19 @@
-// import { PrismaClient } from '@prisma/client'
-// import { prisma } from '../../../../prisma/client/PrismaClient'
+import { user } from '@prisma/client'
+import { prisma } from '../../../../prisma/client/PrismaClient'
+import { CreateUserDTO } from '../../dto/CreateUserDTO'
 
-// prisma.user
+export class CreateUserUseCase {
+
+    async execute({name, email, password}: CreateUserDTO): Promise<user> {
+
+        if (!name || !email || !password) throw new Error("Necessario preencher todas as informações")
+
+        const userAlreadyExist = await prisma.user.findUnique({ where: { email }})
+
+        if (userAlreadyExist) throw new Error("Usuario ja existente")
+
+        const user = await prisma.user.create({data: {name, email, password}})
+
+        return user
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { routes } from './routes/index.routes';
 
 const app = express();
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3000, ()=> console.log("Server running on port 3000"));
